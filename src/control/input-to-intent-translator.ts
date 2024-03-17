ad0da51f479b70e2efaedc3658c52cf150fade7c
Atalykis <cyril1256@hotmail.fr>
2024-03-17 13:27:17 +0100

    input transltor refacto

--- src/control/input-to-intent-translator.ts
+++ src/control/input-to-intent-translator.ts
@@ -3,35 +3,35 @@ import { CanvasRenderer } from "../renderer/implementation/canvas-renderer";
 import { Position } from "../shared/position";
 
 export interface PlayerIntent {
-  intent: string
+  intent: string;
 }
 
 export class InputToIntentTranslator {
-  intents: PlayerIntent[] = []
-  game: Game
-  constructor(private readonly renderer: CanvasRenderer){
-    this.game = this.renderer.game
+  intents: PlayerIntent[] = [];
+  game: Game;
+  constructor(private readonly renderer: CanvasRenderer) {
+    this.game = this.renderer.game;
   }
 
-  async translateClickInput(position: Position): Promise<void>{
-    if(await this.playerDidClickOnTower(position)){
-      this.intents.push({intent : "TowerClicked"})
-      console.log(this.intents)
-      return
+  async translateClickInput(position: Position): Promise<void> {
+    if (await this.playerDidClickOnTower(position)) {
+      this.intents.push({ intent: "TowerClicked" });
+      return;
     }
-    this.intents.push({intent : "No Intent"})
-    console.log(this.intents)
+    this.intents.push({ intent: "No Intent" });
   }
 
-  async playerDidClickOnTower(mousePosition: Position){
-    const state = await this.game.getState()
-    console.log('tower :' ,state.towers[0].position)
-    for(const tower of state.towers){
-      const mouseOnTowerX = (mousePosition.x > tower.position.x  && mousePosition.x < tower.position.x +5)
-      const mouseOnTowerY = (mousePosition.y < tower.position.y && mousePosition.y > tower.position.y - (5* (16/7.53)))
-      const mouseOnTower =  mouseOnTowerX && mouseOnTowerY
-      if(mouseOnTower) return true
+  async playerDidClickOnTower(mousePosition: Position) {
+    const state = await this.game.getState();
+    for (const tower of state.towers) {
+      const mouseOnTowerX =
+        mousePosition.x > tower.position.x &&
+        mousePosition.x < tower.position.x + 5;
+      const mouseOnTowerY =
+        mousePosition.y < tower.position.y &&
+        mousePosition.y > tower.position.y - 5 * (16 / 7.53);
+      const mouseOnTower = mouseOnTowerX && mouseOnTowerY;
+      if (mouseOnTower) return true;
     }
   }
-
 }
--- src/control/mouse-event-handler.ts
+++ src/control/mouse-event-handler.ts
@@ -1,19 +1,32 @@
-import { CanvasRenderer } from "../renderer/implementation/canvas-renderer"
-import { InputToIntentTranslator } from "./input-to-intent-translator"
+import { CanvasRenderer } from "../renderer/implementation/canvas-renderer";
+import { Position } from "../shared/position";
+import { InputToIntentTranslator } from "./input-to-intent-translator";
 
-export class MouseEventHandler{
-  constructor(private readonly renderer: CanvasRenderer, private readonly inputToIntentTranslator: InputToIntentTranslator){
-    window.addEventListener("click", (event) => this.onClick(event))
+export class MouseEventHandler {
+  constructor(
+    private readonly renderer: CanvasRenderer,
+    private readonly inputToIntentTranslator: InputToIntentTranslator
+  ) {
+    window.addEventListener("click", (event) => this.onClick(event));
   }
 
-  onClick(event: MouseEvent) : void{
-    const {offsetLeft, offsetTop} = this.renderer.canvas
-    const {x, y} = event
-    const { width, height } = this.renderer.container.getBoundingClientRect()
-    const pixelPosition = {x: x-offsetLeft, y: y-offsetTop}
-    const relativePosition = {x: pixelPosition.x/(width/100), y: 100 - pixelPosition.y/(height/100) }
-    console.log(relativePosition)
-    this.inputToIntentTranslator.translateClickInput(relativePosition)
+  onClick(event: MouseEvent): void {
+    const mousePosition = { x: event.x, y: event.y };
+    const relativePosition = this.pixelPositionToRelative(mousePosition);
+    this.inputToIntentTranslator.translateClickInput(relativePosition);
   }
 
+  pixelPositionToRelative(position: Position) {
+    const { offsetLeft, offsetTop } = this.renderer.canvas;
+    const { width, height } = this.renderer.container.getBoundingClientRect();
+    const pixelPosition = {
+      x: position.x - offsetLeft,
+      y: position.y - offsetTop,
+    };
+    const relativePosition = {
+      x: pixelPosition.x / (width / 100),
+      y: 100 - pixelPosition.y / (height / 100),
+    };
+    return relativePosition;
+  }
 }
