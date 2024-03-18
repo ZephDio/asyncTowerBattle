68d518e04c1a32026be46188ebd6ab2e791405b7
Zeph <nicolasroure.dev@gmail.com>
2024-03-18 11:56:13 +0100

    rescal on  normalized space

--- src/engine/path/domain/path.ts
+++ src/engine/path/domain/path.ts
@@ -1,12 +1,13 @@
+import { PercentToReal } from "../../../renderer/implementation/canvas-renderer";
 import { Position } from "../../../shared/position";
 
 export type PathNode = Position
 
-export class Path{
-    constructor(public nodes : PathNode[], public type: string){}
+export class Path {
+    constructor(public nodes: PathNode[], public type: string) { }
 }
 
 
-export const PathFixture= {
-    default : new Path([{ x : 10 , y : 90}, { x: 10, y : 10}, { x : 40, y: 40},{ x : 55, y: 20},{ x : 90 , y : 10}], "normal")
+export const PathFixture = {
+    default: new Path([{ x: 10, y: 90 }, { x: 10, y: 10 }, { x: 40, y: 40 }, { x: 55, y: 20 }, { x: 90, y: 10 }].map((position => PercentToReal(position))), "normal")
 }
--- src/renderer/implementation/canvas-renderer.ts
+++ src/renderer/implementation/canvas-renderer.ts
@@ -8,36 +8,37 @@ import { Size } from "../../shared/size";
 import { Renderer } from "../renderer";
 import { Resources } from "../resources";
 import { Drawable, PathDrawable, TowerDrawable, UnitEntityDrawable } from "./drawable";
-export const proportion = (16/7.53)
+export const proportion = (16 / 7.53)
 
-export class CanvasRenderer implements Renderer{
+export class CanvasRenderer implements Renderer {
 
     public container: HTMLElement
     public canvas: HTMLCanvasElement
-    public context : CanvasRenderingContext2D
-    constructor(public game : Game){
+    public context: CanvasRenderingContext2D
+    constructor(public game: Game) {
         this.container = document.getElementById('container') as HTMLElement
         this.canvas = document.getElementById('canvas') as HTMLCanvasElement
         const context = this.canvas.getContext("2d")
-        if(!context) throw new Error()
+        if (!context) throw new Error()
         this.context = context
     }
 
     async draw() {
-       const state = await this.game.getState()
-       const drawableState = this.stateToDrawable(state)
-       for(const drawable of drawableState){
-        drawable.draw(this.context)
+        const state = await this.game.getState()
+        const drawableState = this.stateToDrawable(state)
+        this.drawBackGround()
+        for (const drawable of drawableState) {
+            drawable.draw(this.context)
         }
     }
-    
-    getCanvasPosition(position : Position){
+
+    getCanvasPosition(position: Position) {
         const x = (this.canvas.width / 100) * position.x
-        const y = (this.canvas.height / 100) *  (100 - position.y)
-        return { x , y }
+        const y = this.canvas.height - (this.canvas.width / 100) * position.y// !
+        return { x, y }
     }
 
-   stateToDrawable(state: GameState){
+    stateToDrawable(state: GameState) {
         const drawables: Drawable[] = []
         drawables.push(...state.towers.map((tower) => this.towerToTowerDrawable(tower)))
         drawables.push(this.pathToPathDrawable(state.path))
@@ -45,37 +46,37 @@ export class CanvasRenderer implements Renderer{
         drawables.sort((drawableA, drawableB) => drawableA.drawPriority - drawableB.drawPriority)
         return drawables
     }
-    
-    towerToTowerDrawable(tower: Tower){
+
+    towerToTowerDrawable(tower: Tower) {
         const position = this.getCanvasPosition(tower.position)
-        const { width , height } = Resources.tower[tower.type].size as Size
+        const { width, height } = Resources.tower[tower.type].size as Size
         const size = this.getCanvasSize(width, height)
         return new TowerDrawable(position, size, tower.type)
     }
 
-    pathToPathDrawable(path: Path){
+    pathToPathDrawable(path: Path) {
         const relativeNodes = [...path.nodes.map((node) => this.getCanvasPosition(node))]
         return new PathDrawable(path.type, relativeNodes)
     }
 
-    unitToDrawable(unit: UnitEntity<Unit>){
+    unitToDrawable(unit: UnitEntity<Unit>) {
         const position = this.getCanvasPosition(unit.position)
-        const { width , height } = Resources.unit[unit.unitType].size as Size
+        const { width, height } = Resources.unit[unit.unitType].size as Size
         const size = this.getCanvasSize(width, height)
         return new UnitEntityDrawable(position, size, unit.unitType)
     }
 
 
-    drawBackGround(){
+    drawBackGround() {
         this.context.fillStyle = 'grey'
-        this.context.fillRect(0,0,this.canvas.width,this.canvas.height)
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
     }
 
 
-    init(){
+    init() {
 
         const render = () => {
-            this.context.clearRect(0,0, this.canvas.width, this.canvas.height)
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
             this.sizeGame()
             this.draw()
             window.requestAnimationFrame(render)
@@ -83,9 +84,9 @@ export class CanvasRenderer implements Renderer{
         render()
     }
 
-    sizeGame(){
-        const { width , height } = this.container.getBoundingClientRect()
-        if(width > height * proportion){
+    sizeGame() {
+        const { width, height } = this.container.getBoundingClientRect()
+        if (width > height * proportion) {
             this.canvas.height = height
             this.canvas.width = height * proportion
             return
@@ -94,11 +95,24 @@ export class CanvasRenderer implements Renderer{
         this.canvas.height = width / proportion
     }
 
-    getCanvasSize(width : number, height : number){
+    getCanvasSize(width: number, height: number) {
         const cWidth = (this.canvas.width / 100) * width
-        const cHeight = (this.canvas.height / 100) * height * proportion  // considered to be the perfect ratio considering Navigator HeadBar
-        return { width : cWidth, height : cHeight}
+        const cHeight = (this.canvas.width / 100) * height
+        return { width: cWidth, height: cHeight }
     }
 
+    static PercentToReal(position: Position) {
+        return {
+            x: position.x,
+            y: position.y / proportion
+        }
+    }
 
+}
+
+export const PercentToReal = (position: Position) => {
+    return {
+        x: position.x,
+        y: position.y / proportion
+    }
 }
